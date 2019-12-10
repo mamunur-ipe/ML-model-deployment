7de64dc0caaf82de112b1d80c68d16f063c50533
mamunur-ipe <57313325+mamunur-ipe@users.noreply.github.com>
2019-12-10 04:01:27 -0600

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,35 @@
+# send email from gmail account  
+def send_email(subject, email_body, sender_email_id_password, sender_email_id="mamunur.uta@gmail.com", receiver_email_id="ornoybuet@yahoo.com"):
+	# import smtplib
+	import smtplib
+	# creates SMTP session 
+	mail = smtplib.SMTP('smtp.gmail.com', 587) 
+
+	mail.ehlo()  
+	# start TLS for security 
+	mail.starttls()
+	# call again .ehlo()
+	mail.ehlo() 
+	  
+	# Authentication 
+	mail.login(sender_email_id, sender_email_id_password) 
+	
+	# combine email subject and email body
+	message = 'Subject: {}\n\n{}'.format(subject, email_body)	
+	
+	# sending the mail 
+	mail.sendmail(sender_email_id, receiver_email_id, message) 
+	  
+	# terminating the session 
+	mail.quit()
+
+
+
+    
+
+
+    
+
+
+
+
